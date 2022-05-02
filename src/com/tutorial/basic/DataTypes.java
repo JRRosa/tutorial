e86d4ad39adcd5f798aeb6d596ca11f981a598b8
JRRosa <jorgereyesrosa@gmail.com>
2022-05-02 09:34:16 -0400

    creating numeric data types.

--- src/com/tutorial/basic/DataTypes.java
+++ src/com/tutorial/basic/DataTypes.java
@@ -2,8 +2,20 @@ package com.tutorial.basic;
 
 public class DataTypes {
 
+	// Java Data Types
+	
+	private static void dataTypeNumeric() {
+		// Integer
+		int numericDataInt = 0;
+		System.out.println("Numeric data Integer: " + numericDataInt);
+		 		
+		Integer numericDataInteger = Integer.valueOf(0);
+		System.out.println("Numeric data Integer: " + numericDataInteger);
+	}
+	
+	
 	public static void main(String[] args) {
-		
+		dataTypeNumeric();
 	}
 
 }
