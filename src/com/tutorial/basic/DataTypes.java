fa829038e362d5cd8b4fd7aeae7aa44d73cb4971
JRRosa <jorgereyesrosa@gmail.com>
2022-05-04 14:30:19 -0400

    create envairoment dev and updated data type numeric

--- src/com/tutorial/basic/DataTypes.java
+++ src/com/tutorial/basic/DataTypes.java
@@ -5,12 +5,32 @@ public class DataTypes {
 	// Java Data Types
 	
 	private static void dataTypeNumeric() {
-		// Integer
+		
+		// byte
+		byte numericByte = 3;
+		System.out.println("Numeric data byte: " + numericByte);
+
+		// short
+		short numericShort = 1616;
+		System.out.println("Numeric data short: " + numericShort);
+
+		// int
 		int numericDataInt = 0;
-		System.out.println("Numeric data Integer: " + numericDataInt);
-		 		
-		Integer numericDataInteger = Integer.valueOf(0);
-		System.out.println("Numeric data Integer: " + numericDataInteger);
+		System.out.println("Numeric data int: " + numericDataInt);
+		
+		// long
+		long numericDataLong = 6464;
+		System.out.println("Numeric data long: " + numericDataLong);
+
+		// float
+		float numericFloat = 32.32f;
+		System.out.println("Numeric data float: " + numericFloat);
+
+		// double
+		double numericDouble = 64.64;
+		System.out.println("Numeric data double: " + numericDouble);
+
+
 	}
 	
 	
